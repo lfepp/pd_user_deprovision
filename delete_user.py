fc0c415c31c75bb85d123445e08d7c31e1847de9
Luke Epp <lucasfepp@gmail.com>
2016-09-15 14:51:29 -0700

    Added logic to delete user from schedules

--- delete_user.py
+++ delete_user.py
@@ -27,6 +27,7 @@
 
 import requests
 import json
+import argparse
 
 
 class PagerDutyREST():
@@ -87,3 +88,122 @@ class PagerDutyREST():
                 'There was an issue with your DELETE request:\nStatus code: {code}\
                 \nError: {error}'.format(code=r.status_code, error=r.text)
             )
+
+
+class DeleteUser():
+    """Class to handle all user deletion logic"""
+
+    def __init__(self, user, access_token):
+        self.user = user
+        self.pd_rest = PagerDutyREST(access_token)
+
+    def get_user_id(self, email):
+        """Get PagerDuty user ID from user email"""
+
+        payload = {
+            'query': email
+        }
+        r = self.pd_rest.get('/users?limit=100', payload)
+        # Handle pagination if over 100 users
+        if r['more']:
+            offset = 100
+            output = r['users']
+            while r['more']:
+                r = self.pd_rest.get(
+                    '/users?limit=100&offset={offset}'.format(offset=offset),
+                    payload
+                )
+                output.append(r['users'])
+                offset += 100
+            r = {
+                'users': output
+            }
+        for user in r['users']:
+            if user['email'] == email:
+                return user['id']
+        raise ValueError(
+            'Could not find user with email {email}'.format(email=email)
+        )
+
+    def list_schedules(self):
+        """Outputs list of all schedules"""
+
+        r = self.pd_rest.get('/schedules?limit=100')
+        # Handle pagination if over 100 schedules
+        if r['more']:
+            offset = 100
+            output = r['schedules']
+            while r['more']:
+                r = self.pd_rest.get(
+                    '/schedules?limit=100&offset={offset}'.format(
+                        offset=offset
+                    )
+                )
+                output.append(r['schedules'])
+                offset += 100
+            r = {
+                'schedules': output
+            }
+        return r['schedules']
+
+    def get_schedule(self, schedule_id):
+        """Get a single schedule"""
+
+        r = self.pd_rest.get('/schedules/{id}'.format(id=schedule_id))
+        return r
+
+    def check_schedule_for_user(self, user_id, schedule):
+        """Check if a schedule contains a particular user"""
+
+        for user in schedule['users']:
+            if user['id'] == user_id:
+                return True
+        return False
+
+    def get_user_index(self, user_id, schedule_layer):
+        """Get the index of a user on a schedule layer"""
+
+        for i, user in enumerate(schedule_layer['users']):
+            if user['id'] == user_id:
+                return i
+        return None
+
+    def remove_user_from_layer(self, index, schedule_layer):
+        """Remove a user from a schedule layer"""
+
+        del schedule_layer['users'][index]
+        return schedule_layer
+
+    def cache_schedule(self, schedule, cache):
+        """Adds current schedule to the cache of affected schedules"""
+
+        cache.append({
+            'id': schedule['id'],
+            'name': schedule['name']
+        })
+        return cache
+
+    def delete_user(self, user_id):
+        """Delete user from PagerDuty"""
+
+        r = self.pd_rest.delete('/users/{id}'.format(id=user_id))
+        return r
+
+
+def main(access_token, user_email):
+    """Handle command-line logic to delete user"""
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Delete a user')
+    parser.add_argument(
+        '--access-token',
+        help='PagerDuty v2 access token',
+        dest='access_token',
+        required=True
+    )
+    parser.add_argument(
+        '--user-email',
+        help='Email address of user to be deleted',
+        dest='user_email',
+        required=True
+    )
