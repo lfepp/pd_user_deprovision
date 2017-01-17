93aeefbf1690708c39704473cb79c2bbef914eca
Luke Epp <lucasfepp@gmail.com>
2017-01-17 14:48:47 -0800

    Fixes for add user util

--- user_deprovision.py
+++ user_deprovision.py
@@ -44,7 +44,7 @@ class PagerDutyREST():
             'Authorization': 'Token token={token}'.format(token=access_token)
         }
 
-    def get(self, endpoint, payload={}):
+    def get(self, endpoint, payload={}, resource=None):
         """Handle all GET requests"""
 
         url = '{base_url}{endpoint}'.format(
@@ -59,7 +59,9 @@ class PagerDutyREST():
             # Try/except for cases where getting a single resource
             try:
                 if r['more']:
-                    resource = endpoint[1:]
+                    # FIXME: Remove when teams can have total in params
+                    if not resource:
+                        resource = endpoint[1:]
                     payload['offset'] = 100
                     output = r
                     while r['more']:
